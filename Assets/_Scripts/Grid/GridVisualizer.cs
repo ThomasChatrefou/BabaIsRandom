0493a67e2f39c4f9c738575a9d8000da1a4d45a1
Asbel <tchatrefou@gaming.tech>
2023-12-09 18:34:48 +0100

    Removing / commenting old grid useless stuff
    Just not to delete for now but should be a delete very soon !

--- Assets/_Scripts/Grid/GridVisualizer.cs
+++ Assets/_Scripts/Grid/GridVisualizer.cs
@@ -1,21 +1,20 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridVisualizer : MonoBehaviour
 {
-    [SerializeField] private GameObject visualCellPrefab;
-    [SerializeField] private Grid grid;
+    //[SerializeField] private GameObject visualCellPrefab;
+    //[SerializeField] private Grid grid;
 
-    private Vector2Int lastInstanciatedCellCoord = new(-1, -1);
-    public void ShowNearestCellToPosition(Vector3 worldPosition)
-    {
-        Cell nearestCell = grid.GetNearestCell(worldPosition);
+    //private Vector2Int lastInstanciatedCellCoord = new(-1, -1);
+    //public void ShowNearestCellToPosition(Vector3 worldPosition)
+    //{
+    //    Cell nearestCell = grid.GetNearestCell(worldPosition);
 
-        if (nearestCell.gridCoord != lastInstanciatedCellCoord)
-        {
-            GameObject visualCell = Instantiate(visualCellPrefab, nearestCell.worldPosition, Quaternion.identity, transform);
-            visualCell.transform.localPosition += 1e-5f * Vector3.back; // small offset to avoid Z clipping
-            visualCell.transform.localScale = new Vector3(grid.GetCellSizeX(), grid.GetCellSizeY(), 1f);
-        }
-    }
+    //    if (nearestCell.gridCoord != lastInstanciatedCellCoord)
+    //    {
+    //        GameObject visualCell = Instantiate(visualCellPrefab, nearestCell.worldPosition, Quaternion.identity, transform);
+    //        visualCell.transform.localPosition += 1e-5f * Vector3.back; // small offset to avoid Z clipping
+    //        visualCell.transform.localScale = new Vector3(grid.GetCellSizeX(), grid.GetCellSizeY(), 1f);
+    //    }
+    //}
 }
