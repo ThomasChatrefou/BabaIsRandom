5fcac9b2b641afd9ecaff9bb286f4ef0965882a3
ThomasChatrefou <91902256+ThomasChatrefou@users.noreply.github.com>
2023-12-20 10:16:44 +0100

    Add events to debug translator to send translation outputs

--- Assets/_Scripts/Procedural/Translator/ProceduralDebugTranslator.cs
+++ Assets/_Scripts/Procedural/Translator/ProceduralDebugTranslator.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 // Must be a WorldB (MonoB) to be a component
 public class ProceduralDebugTranslator : WorldBehaviour, IProceduralTranslator
 {
+    public event Action<List<string>> OnGraphTranslated;
+    public event Action<List<string>> OnSolutionTranslated;
+
     public void TranslateGraph(List<Node> nodes)
     {
         Debug.Log($"[ProceduralDebugTranslator] translating graph...");
+        List<string> outputLogs = new();
         foreach (Node node in nodes)
         {
             string childrenStr = ConvertIndexesToNames(node.Children, nodes);
             string keysStr = ConvertIndexesToNames(node.Keys, nodes);
-
-            Debug.Log($"[Node {node.AsciiName}] : Children nodes : [" + childrenStr + "]; Keys : [" + keysStr + "]. ");
+            outputLogs.Add($"[Node {node.AsciiName}] : Children nodes : [" + childrenStr + "]; Keys : [" + keysStr + "]. ");
+            Debug.Log(outputLogs[^1]);
         }
+        OnGraphTranslated?.Invoke(outputLogs);
     }
 
     public void TranslateSolutions(List<string> paths)
@@ -24,6 +30,7 @@ public class ProceduralDebugTranslator : WorldBehaviour, IProceduralTranslator
         {
             Debug.Log(path);
         }
+        OnSolutionTranslated?.Invoke(paths);
     }
 
     #region Private
